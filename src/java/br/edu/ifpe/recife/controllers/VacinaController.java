73d82ce10ebb687276417ab1c4519358a996a6c2
leandromelolm <leandrosiqueira.pe@gmail.com>
2021-11-19 21:07:59 -0300

    criando campos requeridos no cadastro de vacina e mensagem de cadastro

--- src/java/br/edu/ifpe/recife/controllers/VacinaController.java
+++ src/java/br/edu/ifpe/recife/controllers/VacinaController.java
@@ -8,8 +8,10 @@ package br.edu.ifpe.recife.controllers;
 import br.edu.ifpe.recife.model.classes.Vacina;
 import br.edu.ifpe.recife.model.dao.ManagerDao;
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
 
 /**
  *
@@ -27,12 +29,15 @@ public class VacinaController {
         this.cadastro = new Vacina();
     }
     
-    public void insert(){
+    public String insert() throws InterruptedException{
     
-    ManagerDao.getCurrentInstance().insert(this.cadastro);
-    
-    this.cadastro = new Vacina();
-    }
+        ManagerDao.getCurrentInstance().insert(this.cadastro);
+        this.cadastro = new Vacina();    
+        FacesContext.getCurrentInstance().addMessage(null,
+                new FacesMessage(FacesMessage.SEVERITY_INFO, "OK","Vacina "+this.cadastro.getNome()+" cadastrada com sucesso!")); 
+        //new Thread().sleep(40000);
+        return "index.xhtml";
+    }   
 
     public Vacina getCadastro() {
         return cadastro;
@@ -40,7 +45,6 @@ public class VacinaController {
 
     public void setCadastro(Vacina cadastro) {
         this.cadastro = cadastro;
-    }
-    
+    }   
     
 }
