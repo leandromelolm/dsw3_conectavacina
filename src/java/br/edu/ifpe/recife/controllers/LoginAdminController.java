678d716ac241adffb82c5c41acf1508e558373b3
leandromelolm <leandrosiqueira.pe@gmail.com>
2022-01-02 17:05:18 -0300

    Correção: página Paciente no perfil admin (atualização de página não repete envio de dados para o bd); criando página de loginAdmin( ainda com mesmas credenciais de profissional)

---
+++ src/java/br/edu/ifpe/recife/controllers/LoginAdminController.java
@@ -0,0 +1,52 @@
+package br.edu.ifpe.recife.controllers;
+
+import br.edu.ifpe.recife.model.classes.ProfissionalEnfermagem;
+import br.edu.ifpe.recife.model.dao.ManagerDao;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
+/**
+ *
+ * @author melo
+ */
+@ManagedBean(name = "lController")
+@SessionScoped
+public class loginController {
+    
+    private ProfissionalEnfermagem logado;
+    
+    public String logar(String email, String senha){
+        
+        try{
+            ProfissionalEnfermagem auxPS = (ProfissionalEnfermagem)ManagerDao.getCurrentInstance().read(
+                    "select ps from ProfissionalEnfermagem ps where ps.email=\""+email
+                            +"\" and ps.senha=\""+senha+"\"", ProfissionalEnfermagem.class).get(0);
+            
+            this.logado = auxPS;
+            
+            return "ps_homeprofissional.xhtml";
+            
+        }catch(IndexOutOfBoundsException in){
+            
+            FacesContext.getCurrentInstance().addMessage("", new FacesMessage(FacesMessage.SEVERITY_ERROR,
+                    "Erro de Autenticação", "emai e/ou senha estão incorretos"));
+            
+            return null;
+        }       
+    }
+    
+    public String logout(){
+        this.logado=null;
+        return "ps_loginprofissiona.xhtml";
+    }
+
+    public ProfissionalEnfermagem getLogado() {
+        return logado;
+    }
+
+    public void setLogado(ProfissionalEnfermagem logado) {
+        this.logado = logado;
+    }
+}
